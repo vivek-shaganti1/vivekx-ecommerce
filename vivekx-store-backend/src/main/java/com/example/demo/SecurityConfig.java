6f03aafc15e660891798916599085d996384c13a
vivek-shaganti1 <shagantivivekgoud@gmail.com>
2026-05-25 23:57:12 +0530

    fixed cors for vercel frontend

--- vivekx-store-backend/src/main/java/com/example/demo/SecurityConfig.java
+++ vivekx-store-backend/src/main/java/com/example/demo/SecurityConfig.java
@@ -61,4 +61,30 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    @Bean
+    public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
+
+        org.springframework.web.cors.CorsConfiguration configuration =
+                new org.springframework.web.cors.CorsConfiguration();
+
+        configuration.setAllowedOrigins(java.util.List.of(
+                "https://vivekx-ecommerce.vercel.app",
+                "https://vivekx-ecommerce-oeln55xi7-shagantivivekgoud-6262s-projects.vercel.app"
+        ));
+
+        configuration.setAllowedMethods(java.util.List.of(
+                "GET", "POST", "PUT", "DELETE", "OPTIONS"
+        ));
+
+        configuration.setAllowedHeaders(java.util.List.of("*"));
+
+        configuration.setAllowCredentials(true);
+
+        org.springframework.web.cors.UrlBasedCorsConfigurationSource source =
+                new org.springframework.web.cors.UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 }
