d3efcb31b75b2916964fcab11811e38208945ccd
vivek-shaganti1 <shagantivivekgoud@gmail.com>
2026-05-26 00:25:15 +0530

    fixed postgres users table

--- vivekx-store-backend/src/main/java/com/example/demo/User.java
+++ vivekx-store-backend/src/main/java/com/example/demo/User.java
@@ -5,8 +5,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.Table;
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
